b99ccb78db1f2b3660a493e4ad3a53f3fa79fb42
Joshkaki00 <josh.kakinuki@students.dominican.edu>
2025-10-15 14:54:57 -0700

    updating interview room

--- src/components/InterviewRoom.tsx
+++ src/components/InterviewRoom.tsx
@@ -87,9 +87,9 @@ export default function InterviewRoom({ roomId, onLeaveRoom, problemTitle }: Int
       <div className="flex-1 hidden lg:grid lg:grid-cols-[280px_1fr_280px] gap-px bg-gray-300 overflow-hidden">
         {/* Left Sidebar */}
         <div className="bg-white flex flex-col">
-          <div className="flex-1 border-b border-gray-300">
-            <ProblemPanel />
-          </div>
+        <div className="hidden lg:block lg:w-1/3 bg-white border-r border-gray-200">
+          <ProblemPanel problemSlug={currentProblemSlug} />
+        </div>
           <div className="h-48">
             <ParticipantsPanel />
           </div>
