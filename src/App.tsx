cbb0a2daa270228179a1df0ca4e2a4a4288f6210
Joshkaki00 <josh.kakinuki@students.dominican.edu>
2025-10-14 20:01:37 -0700

    updated app

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,35 @@
+import { useState } from 'react'
+import { SocketProvider } from './contexts/SocketContext'
 import LandingPage from './components/LandingPage'
-// import InterviewRoom from './components/InterviewRoom'
+import InterviewRoom from './components/InterviewRoom'
+
+type AppState = 'landing' | 'room'
 
 function App() {
-  // TODO: Add state management and routing logic
-  const showLanding = true
+  const [currentPage, setCurrentPage] = useState<AppState>('landing')
+  const [roomId, setRoomId] = useState<string>('')
+
+  const handleJoinRoom = (id: string) => {
+    setRoomId(id)
+    setCurrentPage('room')
+  }
+
+  const handleLeaveRoom = () => {
+    setRoomId('')
+    setCurrentPage('landing')
+  }
 
   return (
-    <div className="min-h-screen">
-      {showLanding && <LandingPage />}
-      {/* {!showLanding && <InterviewRoom />} */}
-    </div>
+    <SocketProvider>
+      <div className="min-h-screen">
+        {currentPage === 'landing' && (
+          <LandingPage onJoinRoom={handleJoinRoom} />
+        )}
+        {currentPage === 'room' && (
+          <InterviewRoom roomId={roomId} onLeaveRoom={handleLeaveRoom} />
+        )}
+      </div>
+    </SocketProvider>
   )
 }
 
