ebc9fa7603107c12df8353e02634b600e2574987
Joshkaki00 <josh.kakinuki@students.dominican.edu>
2025-10-14 20:56:44 -0700

    Add root route to confirm Socket.IO server is running

--- server/index.ts
+++ server/index.ts
@@ -12,6 +12,10 @@ const io = new Server(server, {
   }
 })
 
+app.get('/', (req, res) => {
+  res.send('Socket.IO server is running!')
+})
+
 interface Room {
   id: string
   participants: Set<string>
