113435ba0eb1487d2119f66b8724907b510bee63
Joshkaki00 <josh.kakinuki@students.dominican.edu>
2025-10-16 19:20:36 -0700

    updated

--- server/index.ts
+++ server/index.ts
@@ -1,6 +1,12 @@
 import express from 'express'
 import { createServer } from 'http'
 import { Server } from 'socket.io'
+import fs from 'fs'
+import path from 'path'
+import { fileURLToPath } from 'url'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
 const app = express()
 app.use(express.json())
